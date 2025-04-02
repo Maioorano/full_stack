0e01948319c2f2c0fb0b6da078e9b79b4df5deb1
Maioorano <jptaty07@gmail.com>
2025-04-02 02:43:00 -0300

    .

--- exemplo java/funcao.js
+++ exemplo java/funcao.js
@@ -4,3 +4,28 @@ function repeat() {
         alert("Vc clicou enviar" + num + "vezez!")
     }
 }
+
+function soma() {
+    let num1 = parseInt(document.getElementById("numero_1").value)
+    let num2 = parseInt(document.getElementById("numero_2").value)
+
+    let resultado_soma = num1 + num2
+
+    document.getElementById("resultado").innerHTML = resultado_soma
+}
+
+function calculo() {
+    let num1 = parseInt(document.getElementById("numero_1").value);
+    let num2 = parseInt(document.getElementById("numero_2").value);
+
+    let resultado = 0;
+
+    if (num1 < 0 || num2 < 0) {
+        alert("Por favor, insira números não negativos.");
+        return; // 
+    }
+
+    resultado = num1 + num2;
+
+    document.getElementById("resultado").innerHTML = "Resultado da soma: " + resultado;
+}
