415a5d33b57c55b1c016eb6478a89534ad3fb131
Maioorano <jptaty07@gmail.com>
2025-04-04 20:55:52 -0300

    .

---
+++ aula8/index.js
@@ -0,0 +1,33 @@
+let canvas = document.getElementById('canvas');
+let ctx = canvas.getContext('2d');
+
+let katana = {
+    x: 0,
+    y: 100,
+    raio: 35,
+    img: new Image(),
+    desenha: function () {
+        this.img.src = 'katana.png';
+        ctx.beginPath();
+        ctx.drawImage(this.img, this.x - this.raio, this.y - this.raio, 2 * this.raio, 2 * this.raio);
+        ctx.closePath();
+    }
+}
+
+function animacao() {
+    ctx.clearRect(0, 0, 300, 300)
+    katana.desenha();
+    requestAnimationFrame(animacao)
+}
+animacao();
+document.addEventListener('mousemove', function (evento) {
+    let rect = canvas.getBoundingClientRect();
+    let x_mouse = evento.clientX - rect.left;
+    let y_mouse = evento.clientY - rect.top;
+    console.log(x_mouse, y_mouse);
+    katana.x = x_mouse;
+    katana.y = y_mouse;
+
+    katana.x = Math.max(0, Math.min(x_mouse, canvas.width));
+    katana.y = Math.max(0, Math.min(y_mouse, canvas.height));
+})
