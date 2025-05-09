6639256d5c6ba90fa10313f0d9734f6adfc1412d
Maioorano <jptaty07@gmail.com>
2025-05-09 20:33:01 -0300

    banco de dados

--- Servidor/servidor.js
+++ Servidor/servidor.js
@@ -3,9 +3,18 @@ var express = require('express');
 var colors = require('colors');
 var bodyParser = require('body-parser');
 
+var mongodb = require("mongodb");
+
+const MongoClient = mongodb.MongoClient;
+
+
+const uri = `mongodb+srv://Maiorano:Jpmgato6@@clusterjao.awiwqkh.mongodb.net/?retryWrites=true&w=majority&appName=ClusterJao`;
+
+
+const client = new MongoClient(uri, { useNewUrlParser: true });
 var app = express();
 app.use(express.static('./public'));
-app.use(bodyParser.urlencoded({ extended: false}))
+app.use(bodyParser.urlencoded({ extended: false }))
 app.use(bodyParser.json())
 app.set('view engine', 'ejs')
 app.set('views', './views');
@@ -15,24 +24,57 @@ server.listen(80);
 
 console.log('Servidor rodando ...'.rainbow);
 
-app.post('/', function (requisicao, resposta){
-resposta.redirect('login.html')
+var dbo = client.db("Informações Blog");
+var Post = dbo.collection("Post");
+
+app.post("/cadastrar_usuario", function (req, resp) {
+    var data = { db_nome: req.body.nome, db_login: req.body.login, db_senha: req.body.senha };
+
+    usuarios.insertOne(data, function (err) {
+        if (err) {
+            resp.render('resposta_usuario', { resposta: "Erro ao cadastrar usuário!" })
+        } else {
+            resp.render('resposta_usuario', { resposta: "Usuário cadastrado com sucesso!" })
+        };
+    });
+
+});
+
+app.post("/logar_usuario", function (req, resp) {
+    var data = { db_login: req.body.login, db_senha: req.body.senha };
+
+    usuarios.find(data).toArray(function (err, items) {
+        console.log(items);
+        if (items.length == 0) {
+            resp.render('resposta_usuario', { resposta: "Usuário/senha não encontrado!" })
+        } else if (err) {
+            resp.render('resposta_usuario', { resposta: "Erro ao logar usuário!" })
+        } else {
+            resp.render('resposta_usuario', { resposta: "Usuário logado com sucesso!" })
+        };
+    });
+
+});
+
+
+app.post('/', function (requisicao, resposta) {
+    resposta.redirect('login.html')
 })
 
-app.get('/inicio', function (requisicao, resposta){
-var nome = requisicao.query.info;
-console.log(nome);
+app.get('/inicio', function (requisicao, resposta) {
+    var nome = requisicao.query.info;
+    console.log(nome);
 })
 
-app.post('/inicio', function (requisicao, resposta){
-var data = requisicao.body.data;
-console.log(data);
+app.post('/inicio', function (requisicao, resposta) {
+    var data = requisicao.body.data;
+    console.log(data);
 })
 
-app.get('/cadastro',function (requisicao, resposta){
-var nome = requisicao.query.nome;
-var senha = requisicao.query.senha;
+app.get('/cadastro', function (requisicao, resposta) {
+    var nome = requisicao.query.nome;
+    var senha = requisicao.query.senha;
 
 
-resposta.render('resposta_cadastro', {nome, senha})
+    resposta.render('resposta_cadastro', { nome, senha })
 })
