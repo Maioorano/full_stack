86434b35e43808b74b376f6c8fb0c2b25a678dda
Maioorano <jptaty07@gmail.com>
2025-05-16 22:03:24 -0300

    lab 9

--- Servidor/servidor.js
+++ Servidor/servidor.js
@@ -1,62 +1,72 @@
-var http = require('http');
-var express = require('express');
-var colors = require('colors');
-var bodyParser = require('body-parser');
-
-var mongodb = require("mongodb");
+const http = require('http');
+const express = require('express');
+const colors = require('colors');
+const bodyParser = require('body-parser');
+const mongodb = require("mongodb");
+const path = require('path');
 
 const MongoClient = mongodb.MongoClient;
-
-
 const uri = "mongodb+srv://Maiorano:AHtGibxZyEAXmltq@clusterjao.awiwqkh.mongodb.net/?retryWrites=true&w=majority&appName=ClusterJao";
 
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
 
-const client = new MongoClient(uri, { useNewUrlParser: true });
-var app = express();
+const app = express();
 app.use(express.static('./public'));
-app.use(bodyParser.urlencoded({ extended: false }))
-app.use(bodyParser.json())
-app.set('view engine', 'ejs')
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+app.set('view engine', 'ejs');
 app.set('views', './views');
 
-var server = http.createServer(app);
-server.listen(80);
+client.connect().then(() => {
+  console.log('✅ MongoDB conectado com sucesso'.green);
+  const dbo = client.db("exemplo_bd");
+  const usuarios = dbo.collection("usuarios");
+  const posts = dbo.collection("posts");
 
-console.log('Servidor rodando ...'.rainbow);
-const dbo = client.db("exemplo_bd");
-const usuarios = dbo.collection("usuarios");
+  // ROTA PADRÃO
+  app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, 'views', 'projects.html'));
+  });
 
-app.post("/cadastrar_usuario", function (req, resp) {
-    var data = { db_login: req.body.nome, db_senha: req.body.senha };
-
-    usuarios.insertOne(data, function (err) {
-        if (err) {
-            resp.render('resposta_erro', { resposta: "Erro ao cadastrar usuário!" })
-        } else {
-            resp.render('resposta_cadastro', { nome: req.body.nome, senha: req.body.senha })
-        };
+  // ROTAS USUÁRIOS
+  app.post("/cadastrar_usuario", (req, res) => {
+    const data = { db_login: req.body.nome, db_senha: req.body.senha };
+    usuarios.insertOne(data, err => {
+      if (err) res.render('resposta_erro', { resposta: "Erro ao cadastrar usuário!" });
+      else res.render('resposta_cadastro', { nome: req.body.nome, senha: req.body.senha });
     });
+  });
 
-});
-
-app.post("/logar_usuario", function (req, resp) {
-    var data = { db_login: req.body.login, db_senha: req.body.senha };
-
-    usuarios.find(data).toArray(function (err, items) {
-        console.log(items);
-        if (items.length == 0) {
-            resp.render('resposta_erro', { resposta: "Usuário/senha não encontrado!" })
-        } else if (err) {
-            resp.render('resposta_erro', { resposta: "Erro ao logar usuário!" })
-        } else {
-            resp.render('resposta_usuario', { resposta: "Usuário logado com sucesso!", nome: req.body.login })
-        };
+  app.post("/logar_usuario", (req, res) => {
+    const data = { db_login: req.body.login, db_senha: req.body.senha };
+    usuarios.find(data).toArray((err, items) => {
+      if (err) res.render('resposta_erro', { resposta: "Erro ao logar usuário!" });
+      else if (items.length == 0) res.render('resposta_erro', { resposta: "Usuário/senha não encontrado!" });
+      else res.render('resposta_usuario', { resposta: "Usuário logado com sucesso!", nome: req.body.login });
     });
+  });
 
+  // ROTAS BLOG
+
+  app.get('/blog', async (req, res) => {
+    const postagens = await posts.find().sort({ _id: -1 }).toArray();
+    res.render('blog', { posts: postagens });
+  });
+
+  app.get('/cadastrar_post', (req, res) => {
+    res.sendFile(path.join(__dirname, 'views', 'cadastrar_post.html'));
+  });
+
+  app.post('/cadastrar_post', async (req, res) => {
+    const { titulo, resumo, conteudo } = req.body;
+    await posts.insertOne({ titulo, resumo, conteudo });
+    res.redirect('/blog');
+  });
+
+  // INICIA SERVIDOR
+  http.createServer(app).listen(80, () => {
+    console.log('🚀 Servidor rodando na porta 80'.cyan);
+  });
+}).catch(err => {
+  console.error('❌ Erro ao conectar no MongoDB:', err);
 });
-
-
-
-app.get('/', function (requisicao, resposta) {
-    resposta.redirect('login.html')
-})
